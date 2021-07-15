bddff8297d5a5357423bfac9722aa396d18f17b3
MrEric0229 <wserry7@gmail.com>
2021-07-14 20:38:20 -0500

    commit

--- wnm608_test/index.php
+++ wnm608_test/index.php
@@ -1,19 +1,30 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Sweetie Heart</title>
+	<meta charset="UTF-8">
+	<title>Home</title>
 	<?php include "parts/meta.php"; ?>
-
 </head>
+
+
 <body>
+	
+	<?php include "parts/navbar.php"; ?>	
 
-	<?php include "parts/navbar.php"; ?>
+	<div class="container">
+		<div class="home-pic-container">
+			<div class="home-pic-item">
+				<h1>
+					Lorem ipsum dolor sit amet, <br> consectetur adipiscing elit.
+				</h1>
 
-	<!-- <div class="view-window" style="background-image: url('img/book.jpg');"></div> -->
-
-
+				<button class="btn-style-1 btn-1" onclick="location.href='menu.php'">
+					Shop Now
+				</button>
+			</div>
+		</div>
+	</div>
 
 	<?php include "parts/footer.php" ?>
-	
+
 </body>
 </html>
